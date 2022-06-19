6fc97e1776dc8d9199640d29ba81d46b11909cb9
RTorrella <105312441+RTorrella@users.noreply.github.com>
2022-06-19 01:01:08 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -10,24 +10,19 @@ def order(values: list = None) -> list:
     if values is None:
         values = []
         while len(values) < 10:
-            values.append(input( "Entrer une valeur (int, float ou str) : " ))
+            values.append(input( "Entrer une valeur  : " ))
        
-    
-    liste_Entiers = []
     liste_Floats = []
     liste_Str = []
 
     for i in values:
-        type_val = type(i)
-        if type_val is int :
-            liste_Entiers.append(i)
-        elif type_val is float : 
+        if type(i) is float : 
             liste_Floats.append(i)
-        elif type_val is str :
+        else:
             liste_Str.append(i)
 
-    liste_triée = sorted(liste_Entiers) + sorted(liste_Floats) + sorted(liste_Str)
-    return liste_triée
+    
+    return sorted(liste_Floats) + sorted(liste_Str)
 
 
 def anagrams(words: list = None) -> bool:
@@ -36,27 +31,24 @@ def anagrams(words: list = None) -> bool:
         while len(words) < 2 :
             words.append(input("Entrer un mot : "))
         
-    vérif_1 = sorted(list(words[0]))
-    vérif_2 = sorted(list(words[1]))
+    condition = sorted(list(words[0])) == sorted(list(words[1]))
+    
 
-    if vérif_1 == vérif_2:
+    if condition:
         return True 
     else:
         return False
 
 
 def contains_doubles(items: list) -> bool:
-    items = []
-    while len(items) != 1 :
-        items.append(input("Entrer une valeur : "))
-
+    
     listeSansDoublons = []
 
     for i in items : 
         if i not in listeSansDoublons:
             listeSansDoublons.append(i)
 
-    if items == listeSansDoublons :
+    if items != listeSansDoublons :
         return True
     else:
         return False
@@ -64,32 +56,66 @@ def contains_doubles(items: list) -> bool:
 
 def best_grades(student_grades: dict) -> dict:
     # TODO: Retourner un dictionnaire contenant le nom de l'étudiant ayant la meilleure moyenne ainsi que sa moyenne
-    return {}
+    meilleureMoyenne = {}
+    listeMoyenne = []
+    for nom, note in student_grades.items():
+        moyenne = sum(note) / len(note)
+        
+        if moyenne not in listeMoyenne:
+            listeMoyenne.append(moyenne)
+            
+    meilleureMoyenne = {nom : max(listeMoyenne)}
+
+    return meilleureMoyenne
 
 
 def frequence(sentence: str) -> dict:
     # TODO: Afficher les lettres les plus fréquentes
     #       Retourner le tableau de lettres
 
-    return {}
+    fréquence = {}
+    for lettre in sentence:
+        fréquence[lettre]= sentence.count(lettre)
+
+    tableauLettres = sorted(fréquence, key=fréquence.get, reverse=True)
+    for car in tableauLettres:
+        if fréquence[car] > 5:
+            print(f"Le caractère {car} revient {fréquence[car]} fois.")
+
+
+    return fréquence
 
 
 def get_recipes():
     # TODO: Demander le nom d'une recette, puis ses ingredients et enregistrer dans une structure de données
-    pass
+
+    recette = input("Entrer le nom de votre recette : ")
+    ingrédient = input("Entrer le nom des différents ingrédients. Séparer les par une virgule : ").split(", ")
+
+
+
+    return {recette: ingrédient}
 
 
 def print_recipe(ingredients) -> None:
     # TODO: Demander le nom d'une recette, puis l'afficher si elle existe
-    pass
+    nom = input("Quel est le nom de votre recette ? ")
+
+    if nom in ingredients:
+        print(ingredients[nom])
+    else:
+        print("Cette recette ne fait pas partie de notre livre de recettes.")
+        print(ingredients)
+        
+    
 
 
 def main() -> None:
     print(f"On essaie d'ordonner les valeurs...")
-    order()
+    print(order())
 
     print(f"On vérifie les anagrammes...")
-    anagrams()
+    print(anagrams())
 
     my_list = [3, 3, 5, 6, 1, 1]
     print(f"Ma liste contient-elle des doublons? {contains_doubles(my_list)}")
